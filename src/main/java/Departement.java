39e86f075d9c5d28fe94e15091d8911789f0a84e
SacreDeBirmanie <sebful44@hotmail.fr>
2016-11-29 15:49:47 +0100

    Ajout des import javax.persistence dans la classe Departement et modification attribut

--- src/main/java/Departement.java
+++ src/main/java/Departement.java
@@ -1,8 +1,15 @@
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import java.util.ArrayList;
+
+import static javax.persistence.GenerationType.AUTO;
 
 /**
  * Created by Sébastien on 28/11/2016.
  */
+@Entity
 public class Departement {
     @Id
     @GeneratedValue(strategy=AUTO)
@@ -11,5 +18,10 @@ public class Departement {
     private String nom;
 
     @OneToMany(mappedBy = "departement")
+    private ArrayList<Module> listeDesModules;
+
+    @OneToMany(mappedBy = "departement")
     private ArrayList<Enseignant> listeDesEnseignants;
+
+
 }
